c7215331c2a79bac79b971794fba720ecfeac7fd
andradeviniicius <viniciusdandrade01@gmail.com>
2022-09-30 20:45:37 -0300

    Initial commit for NextJS refac

--- server/src/utils/convertHourStringToMinutes.ts
+++
@@ -1,6 +0,0 @@
-// 18:00 -> 1800
-export default function convertHourStringToMinutes(hourString: String) {
-  const [hours, minutes] = hourString.split(":").map(Number);
-  const minutesAmount = hours * 60 + minutes;
-  return minutesAmount;
-}
--- server/src/utils/convertMinuteStringToHours.ts
+++
@@ -1,8 +0,0 @@
-// 1100 -> 18:20
-
-export function convertMinutesToHourString(minutesAmount: number) {
-    const hour = Math.floor(minutesAmount / 60);
-    const minutes = minutesAmount % 60;
-  
-    return `${String(hour).padStart(2, '0')}:${String(minutes).padStart(2, '0')}`;
-}
