4f439eb2f1996cbe747b947aa1c125829d87324a
andradeviniicius <viniciusdandrade01@gmail.com>
2022-10-06 17:07:17 -0300

    web/ adding dummy data for <GameList/>

--- web/src/components/GameCard/index.tsx
+++ web/src/components/GameCard/index.tsx
@@ -1,21 +1,34 @@
 import Image from "next/image";
-import Image2 from '../../assets/image02.png'
+import Link from "next/link";
+import Image2 from "../../assets/image02.png";
 
 type Props = {
   gameBanner: string;
   gameTitle: string;
+  gameId: any;
   adsCount: string;
 };
 
-export default function GameCard({ gameBanner, gameTitle, adsCount }: Props) {
+export default function GameCard({
+  gameBanner,
+  gameTitle,
+  gameId,
+  adsCount,
+}: Props) {
   return (
-      <a href="" className="relative rounded-lg overflow-hidden">
-        <Image src={Image2} />
+    <div className="relative rounded-lg overflow-hidden">
+      <Link href={`/games/${gameId}`}>
+        <a>
+          <Image src={gameBanner} width={180} height={240} />
 
-        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-          <strong className="font-bold text-white block">{gameTitle}</strong>
-          <span className="text-zinc-300 text-sm block">{adsCount} Ad(s)</span>
-        </div>
-      </a>
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">{gameTitle}</strong>
+            <span className="text-zinc-300 text-sm block">
+              {adsCount} Ad(s)
+            </span>
+          </div>
+        </a>
+      </Link>
+    </div>
   );
 }
--- web/src/components/GameList/index.tsx
+++ web/src/components/GameList/index.tsx
@@ -1,15 +1,53 @@
 import { GameCard } from "../../components";
 type Props = {};
 
+const games = [
+  {
+    gameId: 1,
+    gameTitle: "World Of Warcraft",
+    gameBanner: "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg",
+  },
+  {
+    gameId: 2,
+    gameTitle: "H1Z1",
+    gameBanner: "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg",
+  },
+  {
+    gameId: 3,
+    gameTitle: "Valorant",
+    gameBanner: "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg",
+  },
+  {
+    gameId: 4,
+    gameTitle: "league of legends",
+    gameBanner: "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg",
+  },
+  {
+    gameId: 5,
+    gameTitle: "Fortnite",
+    gameBanner: "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg",
+  },
+  {
+    gameId: 6,
+    gameTitle: "GTA",
+    gameBanner: "https://static-cdn.jtvnw.net/ttv-boxart/516575-285x380.jpg",
+  },
+];
+
 export default function GameList(params: Props) {
   return (
     <div className="grid grid-cols-6 gap-6 mt-16">
-      <GameCard gameBanner={"/web/src/assets/image02.png"} gameTitle="Aaaa" adsCount="2" />
-      <GameCard gameBanner={"/web/src/assets/image02.png"} gameTitle="Aaaa" adsCount="2" />
-      <GameCard gameBanner={"/web/src/assets/image02.png"} gameTitle="Aaaa" adsCount="2" />
-      <GameCard gameBanner={"/web/src/assets/image02.png"} gameTitle="Aaaa" adsCount="2" />
-      <GameCard gameBanner={"/web/src/assets/image02.png"} gameTitle="Aaaa" adsCount="2" />
-      <GameCard gameBanner={"/web/src/assets/image02.png"} gameTitle="Aaaa" adsCount="2" />
+      {games.map((el, index) => {
+        return (
+          <GameCard
+            gameBanner={el.gameBanner}
+            gameTitle={el.gameTitle}
+            gameId={el.gameId}
+            adsCount={'1'}
+            key={index}
+          />
+        );
+      })}
     </div>
   );
 }
