1d32f618f2f5b3a912a10f612c79d5bb11c9a9aa
andradeviniicius <viniciusdandrade01@gmail.com>
2022-10-10 15:47:57 -0300

    web/ implement <Carousel/> in <GameList/>

--- web/src/components/GameCard/index.tsx
+++ web/src/components/GameCard/index.tsx
@@ -15,23 +15,32 @@ export default function GameCard({
   adsCount,
 }: Props) {
   return (
-    <div className="relative rounded-lg overflow-hidden hover:scale-110 transition ease-in-out">
-      <Link href={`/games/${gameId}`}>
-        <a>
-          <Image
-            src={gameBanner.replace("{width}x{height}", "180x240")}
-            width={180}
-            height={240}
-          />
+    <div
+      className="carousel-item text-center relative w-[180] h-60 snap-start"
+    >
+      <div
+        className="h-full w-full aspect-[3/4] block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0 "
+      >
+        <Link href={`/games/${gameId}`}>
+          <a>
+            <Image
+              src={gameBanner.replace("{width}x{height}", "180x240")}
+              width={180}
+              height={240}
+              className="hover:scale-105 transition ease-in-out"
+            />
 
-          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-            <strong className="font-bold text-white block">{gameTitle}</strong>
-            <span className="text-zinc-300 text-sm block">
-              {adsCount} Ad(s)
-            </span>
-          </div>
-        </a>
-      </Link>
+            <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+              <strong className="font-bold text-white block">
+                {gameTitle}
+              </strong>
+              <span className="text-zinc-300 text-sm block">
+                {adsCount} Ad(s)
+              </span>
+            </div>
+          </a>
+        </Link>
+      </div>
     </div>
   );
 }
--- web/src/components/GameList/index.tsx
+++ web/src/components/GameList/index.tsx
@@ -1,23 +1,24 @@
-import { GameCard } from "../../components";
+import { Carousel, GameCard } from "../../components";
 type Props = {
   twitchTopGames?: any;
 };
 
 export default function GameList({ twitchTopGames }: Props) {
-  console.log(twitchTopGames);
   return (
-    <div className="grid grid-cols-6 gap-6 mt-16">
-      {twitchTopGames.data.map((el: any, index: any) => {
-        return (
-          <GameCard
-            gameBanner={el.box_art_url}
-            gameTitle={el.name}
-            gameId={el.id}
-            // adsCount={"1"}
-            key={index}
-          />
-        );
-      })}
+    <div className="2xl:container 2xl:mx-auto 2xl:px-0 py-3 px-10">
+      <Carousel>
+        {twitchTopGames.data.map((el: any, index: any) => {
+          return (
+            <GameCard
+              gameBanner={el.box_art_url}
+              gameTitle={el.name}
+              gameId={el.id}
+              // adsCount={"1"}
+              key={index}
+            />
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
