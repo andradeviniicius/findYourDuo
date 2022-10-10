c4e7c97ee89e213a7cd57f425fd8464c4cd14374
andradeviniicius <viniciusdandrade01@gmail.com>
2022-10-10 15:47:29 -0300

    web/ create <Carousel/> component

--- web/src/components/index.ts
+++ web/src/components/index.ts
@@ -3,6 +3,7 @@ export { default as GameCard } from "./GameCard";
 export { default as GameList } from "./GameList";
 export { default as CreateGame } from "./CreateGame";
 export { default as CreateAdForm } from "./CreateAdForm";
+export { default as Carousel } from "./Carousel";
 
 export { default as SelectInput } from "./Input/SelectInput";
 export { default as DefaultInput } from "./Input/DefaultInput";
