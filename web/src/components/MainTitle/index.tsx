433d114e69db144f7ab33aeecf075165513643a7
andradeviniicius <viniciusdandrade01@gmail.com>
2022-10-16 23:50:42 -0300

    Removing lazy loading from Logo <Image/>

--- web/src/components/MainTitle/index.tsx
+++ web/src/components/MainTitle/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function MainTitle() {
   return (
     <>
-      <Image src={logoNLW} alt="" />
+      <Image priority src={logoNLW} alt="" />
       <h1 className="text-6xl text-white font-black mt-20 text-center">
         Seu<span className="text-transparent bg-duo-gradient bg-clip-text"> DUO </span>está aqui !
       </h1>
