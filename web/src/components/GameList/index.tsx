199d4702cd3a500c0ad73590126867ba22d32eb7
andradeviniicius <viniciusdandrade01@gmail.com>
2023-05-06 22:08:37 -0300

    fix/ adding error message when twitch api throws error

--- web/src/components/GameList/index.tsx
+++ web/src/components/GameList/index.tsx
@@ -1,12 +1,41 @@
-import { Carousel, GameCard } from "../../components";
+import { Carousel, GameCard, Spinner } from "../../components";
 type Props = {
   twitchTopGames?: any;
 };
 
 export default function GameList({ twitchTopGames }: Props) {
+  console.log("error", twitchTopGames);
+
+  if (twitchTopGames.message) {
+    return (
+      <div
+        className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative my-10"
+        role="alert"
+      >
+        <strong className="font-bold">Hey dev! </strong>
+        <span className="block sm:inline">
+          {" "}
+          something went wrong in the Twitch API
+        </span>
+        <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg
+            className="fill-current h-6 w-6 text-red-500"
+            role="button"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <title>Close</title>
+            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+          </svg>
+        </span>
+      </div>
+    );
+  }
+
   return (
-      <Carousel>
-        {twitchTopGames.data.map((el: any, index: any) => {
+    <Carousel>
+      {twitchTopGames.data ? (
+        twitchTopGames.data.map((el: any, index: any) => {
           return (
             <GameCard
               gameBanner={el.box_art_url}
@@ -16,7 +45,10 @@ export default function GameList({ twitchTopGames }: Props) {
               key={index}
             />
           );
-        })}
-      </Carousel>
+        })
+      ) : (
+        <Spinner />
+      )}
+    </Carousel>
   );
 }
